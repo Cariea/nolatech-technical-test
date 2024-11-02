eeb51e8961a96a9e44c0efd13943e25310a6ec49
Carmelo Naim <carmelonaim30@gmail.com>
2024-11-02 19:55:54 -0400

    feat: add schema guard

---
+++ src/middlewares/schema-guard.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express'
+import { z, ZodError } from 'zod'
+
+import { STATUS } from '../utils/constants'
+
+export function validateData(schema: z.ZodObject<any, any>) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body)
+      next()
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((issue: any) => ({
+          message: `${issue.path.join('.')} is ${issue.message}`
+        }))
+        res
+          .status(STATUS.BAD_REQUEST)
+          .json({ error: 'Invalid data', details: errorMessages })
+      } else {
+        res
+          .status(STATUS.INTERNAL_SERVER_ERROR)
+          .json({ error: 'Internal Server Error' })
+      }
+    }
+  }
+}
