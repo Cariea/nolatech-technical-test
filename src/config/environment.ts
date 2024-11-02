6fa1f5dfcabae1cc26b5c2f0e0366e9ae4076e47
Carmelo Naim <carmelonaim30@gmail.com>
2024-11-02 19:39:00 -0400

    chore: add env vars

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,5 +2,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-// Servers
+// Environment
 export const PORT = process.env.PORT
+export const MONGO_URL = process.env.MONGO_URL
+export const JWT_SECRET = process.env.JWT_SECRET
+export const JWT_EXPIRATION = process.env.JWT_EXPIRATION
