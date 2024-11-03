79fd79ea1b7541b289353e2f1e298bb1651430e8
Carmelo Naim <carmelonaim30@gmail.com>
2024-11-03 12:01:17 -0400

    feat: add create user service for admin creation

---
+++ src/services/user.service.ts
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt'
+import { MongooseRepository } from '../repositories/mongoose-repository'
+import { UserDocument, UserModel } from '../models/user/user.model'
+import { Roles } from '../models/user/enums/roles.enum'
+export class UserService {
+  private readonly userRepository: MongooseRepository<UserDocument>
+
+  constructor() {
+    this.userRepository = new MongooseRepository(UserModel)
+  }
+
+  async create(
+    name: string,
+    email: string,
+    password: string
+  ): Promise<UserDocument> {
+    const hashedPassword = await bcrypt.hash(password, 10)
+    return this.userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+      role: Roles.ADMIN
+    })
+  }
+}
