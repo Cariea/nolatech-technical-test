c4f6a953a3ebece5f3298c5104d34ba239e7afbe
Carmelo Naim <carmelonaim30@gmail.com>
2024-11-03 14:46:58 -0400

    feat: add findOne Employee route

--- src/routes/employee/employee.routes.ts
+++ src/routes/employee/employee.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { validateData } from '../../_middlewares/schema-guard'
+import { validateData, validateParams } from '../../_middlewares/schema-guard'
 import { createEmployeeDto } from '../../controllers/employee/dto/create-employee.dto'
 import { EmployeeController } from '../../controllers/employee/employee.controller'
 import { hasAuthorization } from '../../_middlewares/auth-guard'
 import { Roles } from '../../models/user/enums/roles.enum'
+import { userIdSchema } from '../../controllers/employee/dto/get-by-id.employee.dto'
 
 const router = Router()
 const employeeController = new EmployeeController()
@@ -11,6 +12,14 @@ const employeeController = new EmployeeController()
 router.get('/', hasAuthorization([Roles.ADMIN]), (req, res) =>
   employeeController.findAll(req, res)
 )
+
+router.get(
+  '/:id',
+  hasAuthorization([Roles.ADMIN]),
+  validateParams(userIdSchema),
+  (req, res) => employeeController.findOne(req, res)
+)
+
 router.post(
   '/create',
   hasAuthorization([Roles.ADMIN]),
