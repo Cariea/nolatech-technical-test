b0148ae3497efc351b80329779ce4b4f314b0eb1
Carmelo Naim <carmelonaim30@gmail.com>
2024-11-03 12:49:40 -0400

    feat: add employe service

---
+++ src/services/employee.service.ts
@@ -0,0 +1,41 @@
+import bcrypt from 'bcrypt'
+import { MongooseRepository } from '../_repositories/mongoose-repository'
+import {
+  EmployeeDocument,
+  EmployeeModel
+} from '../models/employee/employee.model'
+import { Departments } from '../models/employee/enums/departments.enum'
+import { Roles } from '../models/user/enums/roles.enum'
+import { UserDocument, UserModel } from '../models/user/user.model'
+import { Types } from 'mongoose'
+export class UserService {
+  private readonly employeeRepository: MongooseRepository<EmployeeDocument>
+  private readonly userRepository: MongooseRepository<UserDocument>
+
+  constructor() {
+    this.employeeRepository = new MongooseRepository(EmployeeModel)
+    this.userRepository = new MongooseRepository(UserModel)
+  }
+
+  async create(
+    name: string,
+    email: string,
+    password: string,
+    department: Departments
+  ): Promise<EmployeeDocument> {
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = await this.userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+      role: Roles.EMPLOYEE
+    })
+
+    const employee = await this.employeeRepository.create({
+      user: user._id as Types.ObjectId,
+      department
+    })
+
+    return employee
+  }
+}
