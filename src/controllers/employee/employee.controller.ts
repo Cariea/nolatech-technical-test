c0529f1f7e35feb3fbf834767a2ca741074134b4
Carmelo Naim <carmelonaim30@gmail.com>
2024-11-03 14:21:19 -0400

    feat: add find all employees on controller

--- src/controllers/employee/employee.controller.ts
+++ src/controllers/employee/employee.controller.ts
@@ -25,4 +25,15 @@ export class EmployeeController {
         .json({ message: error instanceof Error ? error.message : error })
     }
   }
+
+  async findAll(_req: Request, res: Response): Promise<void> {
+    try {
+      const employees = await this.employeeService.findAll()
+      res.status(200).json({ employees })
+    } catch (error) {
+      res
+        .status(400)
+        .json({ message: error instanceof Error ? error.message : error })
+    }
+  }
 }
