3afbd72e5a2ea7f898b28f827ed25e0a74a51229
Carmelo Naim <carmelonaim30@gmail.com>
2024-11-03 14:42:48 -0400

    feat: add params validator middleware

--- src/_middlewares/schema-guard.ts
+++ src/_middlewares/schema-guard.ts
@@ -24,3 +24,25 @@ export function validateData(schema: z.ZodObject<any, any>) {
     }
   }
 }
+
+export function validateParams(schema: z.ZodObject<any, any>) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.params)
+      next()
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((issue: any) => ({
+          message: `${issue.path.join('.')} is ${issue.message}`
+        }))
+        res
+          .status(STATUS.BAD_REQUEST)
+          .json({ error: 'Invalid data', details: errorMessages })
+      } else {
+        res
+          .status(STATUS.INTERNAL_SERVER_ERROR)
+          .json({ error: 'Internal Server Error' })
+      }
+    }
+  }
+}
