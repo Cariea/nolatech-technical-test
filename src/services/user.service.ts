0cc400247804bc50ba3424448ef288238e55b4fb
Carmelo Naim <carmelonaim30@gmail.com>
2024-11-03 18:41:57 -0400

    feat: add findOne user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,6 +2,7 @@ import bcrypt from 'bcrypt'
 import { MongooseRepository } from '../_repositories/mongoose-repository'
 import { UserDocument, UserModel } from '../models/user/user.model'
 import { Roles } from '../models/user/enums/roles.enum'
+import { Types } from 'mongoose'
 export class UserService {
   private readonly userRepository: MongooseRepository<UserDocument>
 
@@ -21,5 +22,9 @@ export class UserService {
       password: hashedPassword,
       role: Roles.ADMIN
     })
+  }
+
+  async findById(id: Types.ObjectId): Promise<UserDocument | null> {
+    return this.userRepository.findOne({ _id: id })
   }
 }
