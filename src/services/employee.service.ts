6de267c488c3db36cdb128c6b89797647cef2806
Carmelo Naim <carmelonaim30@gmail.com>
2024-11-03 15:43:16 -0400

    feat: add employee update service

--- src/services/employee.service.ts
+++ src/services/employee.service.ts
@@ -8,6 +8,9 @@ import { Departments } from '../models/employee/enums/departments.enum'
 import { Roles } from '../models/user/enums/roles.enum'
 import { UserDocument, UserModel } from '../models/user/user.model'
 import { Types } from 'mongoose'
+import { UpdateEmployeeDto } from '../controllers/employee/dto/update-employee.dto'
+import { StatusError } from '../utils/responses/status-error'
+import { STATUS } from '../utils/constants'
 export class EmployeeService {
   private readonly employeeRepository: MongooseRepository<EmployeeDocument>
   private readonly userRepository: MongooseRepository<UserDocument>
@@ -49,5 +52,57 @@ export class EmployeeService {
 
   async findOne(id: string): Promise<EmployeeDocument | null> {
     return this.employeeRepository.findById(id, ['user'], ['password'])
+  }
+
+  async update(
+    id: string,
+    data: UpdateEmployeeDto
+  ): Promise<EmployeeDocument | null> {
+    const employee = await this.employeeRepository.findById(id)
+    if (!employee) {
+      throw new StatusError({
+        statusCode: STATUS.NOT_FOUND,
+        message: 'Empleado no encontrado'
+      })
+    }
+
+    const user = await this.userRepository.findById(
+      employee.user._id as unknown as string
+    )
+
+    if (!user) {
+      throw new StatusError({
+        statusCode: STATUS.NOT_FOUND,
+        message: 'Usuario no encontrado'
+      })
+    }
+
+    if (data.email && data.email !== user.email) {
+      const userWithSameEmail = await this.userRepository.findOne({
+        email: data.email
+      })
+
+      if (userWithSameEmail) {
+        throw new StatusError({
+          statusCode: STATUS.CONFLICT,
+          message: 'Ya existe un usuario con ese email'
+        })
+      }
+    }
+
+    await this.userRepository.update(
+      employee.user._id as unknown as string,
+      data
+    )
+
+    await this.employeeRepository.update(id, data)
+
+    const updatedEmployee = await this.employeeRepository.findById(
+      id,
+      ['user'],
+      ['password']
+    )
+
+    return updatedEmployee
   }
 }
